4e199a95ac8cd59e92a8ef17f90d9a6721fea1bf
이용우 <qhtm008@naver.com>
2025-11-10 16:02:42 +0900

    feature/monster

---
+++ src/main/java/monster/Floor3Boss.java
@@ -0,0 +1,11 @@
+package monster;
+
+public class Floor3Boss extends Monster{
+    public Floor3Boss(){
+        super("뷰티풀M",
+                300,
+                100,
+                new String[]{"퇴실문자 보내기"},
+                3);
+    }
+}
