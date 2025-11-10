83601fe315844d6a0850ba592f6dafd26a586a53
이용우 <qhtm008@naver.com>
2025-11-10 16:44:37 +0900

    feature/battle-system

---
+++ src/main/java/battle/BattleSystem.java
@@ -0,0 +1,67 @@
+package battle;
+
+import monster.Monster;
+import user.Player;
+
+import java.util.Scanner;
+
+public class BattleSystem {
+    private Player player;
+    private Monster monster;
+    private int stage;
+
+    public BattleSystem(Player player, Monster monster, int stage) {
+        this.player = player;
+        this.monster = monster;
+        this.stage = stage;
+    }
+
+    public void startBattle() {
+        //1. 만남
+        System.out.println(monster.toString());
+
+        //2~3번을 몬스터가 죽을 때까지 반복
+        while(monster.isAlive()) {
+            performMonsterAttack();
+            //TODO:player의 피가 0이 되면 종료
+            performPlayerTurn();
+        }
+
+        displayBattleResult();
+    }
+
+    private void performMonsterAttack() {
+        //2. 몬스터 공격
+        monster.printStatus();
+        int n=monster.getSkills().length;
+        String skillUsed = monster.getSkills()[(int)(Math.random()*n)];
+        System.out.println(monster.getName() + "의 " + skillUsed + " 사용!");
+        player.printStatus();
+    }
+
+    private void performPlayerTurn() {
+        //3. 사람 공격
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. 공격 , 2. 물약회복");
+        //3-1. mp소모 공격
+        int choice = scanner.nextInt();
+        if(choice == 1) {
+            player.useSkill();
+            monster.takeDamage(1);
+            player.printStatus();
+            monster.printStatus();
+        }else if(choice == 2) {
+            //TODO:물약 소모
+            player.printStatus();
+            monster.printStatus();
+        }
+    }
+
+    private void displayBattleResult() {
+        if(monster.isAlive()) {
+            System.out.println("패배했습니다...");
+        } else {
+            System.out.println("승리했습니다!");
+        }
+    }
+}
--- src/main/java/monster/Floor2Boss.java
+++ src/main/java/monster/Floor2Boss.java
@@ -3,7 +3,7 @@ package monster;
 public class Floor2Boss extends Monster{
     public Floor2Boss(){
         super("타바코C",
-                150,
+                1,
                 70,
                 new String[]{"담배냄새 풍기기"},
                 2);
