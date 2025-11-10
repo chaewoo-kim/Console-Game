f4ef0f511ae5d82eb5f8065be43a0b50caa16be8
chaewookim <chaewookim2877@gmail.com>
2025-11-10 15:44:14 +0900

    feat 상점 판매 기능 추가

--- src/main/java/items/Shop.java
+++ src/main/java/items/Shop.java
@@ -2,10 +2,7 @@ package items;
 
 import user.Player;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Shop {
 
@@ -45,29 +42,79 @@ public class Shop {
         });
     }
 
-//    public void sell(Player player) {
-//
-//        Item[] playerItems = {items.armor.get(1), items.weapon.get(2), items.supplies.get(2)}; // 나중에 player.items로 바꿔야함
-//        Scanner sc = new Scanner(System.in);
-//
-//        int money = 5; // 이거 나중에 player.money로 바꿔야 함
-//        String itemName = "";
-//
-//        Arrays.stream(playerItems).forEach(item -> {
-//            System.out.println("이름: " + item.getName() + ", 판매 가격: " + item.getCost());
-//        });
-//
-//        System.out.print("판매할 아이템의 이름: ");
-//        itemName = sc.nextLine();
-//
-//        // 받은 아이템 이름 비교해서 돈 빼기
-//        // 돈 없으면 못 쓰게
-//        // 알림 문구 출력
-//
-//    }
+    public void sell(Player player) {
+
+        List<Item> playItemList = new ArrayList<>(
+                Arrays.asList(
+                        items.weapon.get(1),
+                        items.armor.get(3),
+                        items.supplies.get(1)
+                )
+        );
+        Scanner sc = new Scanner(System.in);
+
+        int money = 5; // 이거 나중에 player.money로 바꿔야 함
+        int input = -1;
+        String itemName = "";
+        int itemCost = 0;
+        int itemNum = 0;
+
+        while (input != 0) {
+
+            if (playItemList.isEmpty()) {
+                System.out.println("판매할 물건 없음");
+                break;
+            }
+
+            System.out.println("현재 돈: " + money);
+
+            for (int i = 0; i < playItemList.size(); i++) {
+                System.out.println("아이템 번호: " + i);
+                System.out.println("아이템 이름: " + playItemList.get(i).getName());
+                System.out.println("아이템 가격: " + playItemList.get(i).getCost());
+            }
+
+            System.out.print("판매할 아이템의 번호: ");
+            itemNum = sc.nextInt();
+            itemName = playItemList.get(itemNum).getName();
+
+            for (Map.Entry<Integer, Item> entry : items.weapon.entrySet()) {
+                if (entry.getValue().getName().equals(itemName)) {
+                    itemCost = entry.getValue().getCost();
+                    break;
+                }
+            }
+
+            for (Map.Entry<Integer, Item> entry : items.armor.entrySet()) {
+                if (entry.getValue().getName().equals(itemName)) {
+                    itemCost = entry.getValue().getCost();
+                    break;
+                }
+            }
+
+            for (Map.Entry<Integer, Item> entry : items.supplies.entrySet()) {
+                if (entry.getValue().getName().equals(itemName)) {
+                    itemCost = entry.getValue().getCost();
+                    break;
+                }
+            }
+
+            playItemList.remove(itemNum);
+            money += itemCost;
+            System.out.println("판매 완료");
+            System.out.println("추가 판매: 1, 판매 종료: 0");
+            System.out.print("입력: ");
+            input = sc.nextInt();
+
+        }
+
+        // Player 객체에 돈 업데이트 해야함
+
+    }
 
     public static void main(String[] args) {
         Shop shop = new Shop();
-        shop.buy();
+//        shop.buy();
+        shop.sell(new Player("chaewookim"));
     }
 }
