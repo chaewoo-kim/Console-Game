9534ac25a5d5634bdc5fb396684db66e0de773d4
chaewookim <chaewookim2877@gmail.com>
2025-11-11 17:36:00 +0900

    #19 GameManager updated

--- src/main/java/monster/Floor2Boss.java
+++ src/main/java/monster/Floor2Boss.java
@@ -4,7 +4,7 @@ public class Floor2Boss extends Monster{
     public Floor2Boss(){
         super("타바코C",
                 1,
-                70,
+                10,
                 new String[]{"담배냄새 풍기기"},
                 2);
     }
--- src/main/java/monster/Floor4Boss.java
+++ src/main/java/monster/Floor4Boss.java
@@ -4,7 +4,7 @@ public class Floor4Boss extends Monster{
     public Floor4Boss(){
         super("가스G",
                 2,
-                120,
+                10,
                 new String[]{"가스 방출"},
                 4);
     }
